95692765a89e2fe36cdb8ac485231ab18bcb408f
Alexandre <alexandre.annic1@gmail.com>
2019-02-15 19:18:50 +0100

    Create RadioSet module

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {makeStyles} from "@material-ui/styles"
 import {Cv} from "./Cv/Cv"
 import Icon from "@material-ui/core/Icon/Icon"
 import {useChangeLang, useLang} from "./i18n/I18nContext"
-import Radio from "@material-ui/core/Radio/Radio"
 import Button from "@material-ui/core/Button/Button"
 import Tooltip from "@material-ui/core/Tooltip/Tooltip"
+import {RadioSet, RadioSetItem} from "./Cv/RadioSet"
 
 const useStyles = makeStyles(t => ({
   '@global': {
@@ -44,12 +44,13 @@ const useStyles = makeStyles(t => ({
   },
   langs: {
     flex: 1,
+    marginLeft: '0px !important',
   },
   btn_i: {
     marginRight: t.spacing.unit,
   },
   body: {
-    height: '29.7cm',
+    height: '30cm',//'29.7cm',
     background: t.palette.background.paper,
     marginTop: t.spacing.unit,
     marginBottom: t.spacing.unit * 2,
@@ -66,27 +67,10 @@ const App = () => {
   return (
     <main className={css.root}>
       <div className={css.actions}>
-        <div className={css.langs}>
-          <Button variant="outlined" color="primary" onClick={() => changeLang('fr')}
-                  style={{borderTopRightRadius: 0, borderBottomRightRadius: 0, borderRight: 'none'}}>
-            <Radio checked={lang === 'fr'} color="primary" style={{padding: 0, marginLeft: -4, marginRight: 6}}/>
-            Fr
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => changeLang('en')}
-                  style={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0}}>
-            <Radio checked={lang === 'en'} color="primary" style={{padding: 0, marginLeft: -4, marginRight: 6}}/>
-            En
-          </Button>
-        </div>
-        {/*<RadioGroup*/}
-        {/*className={css.langs}*/}
-        {/*name="pick-lang"*/}
-        {/*value={lang}*/}
-        {/*onChange={e => changeLang(e.target.value)}*/}
-        {/*>*/}
-        {/*<FormControlLabel value="fr" control={<Radio/>} label="Fr"/>*/}
-        {/*<FormControlLabel value="en" control={<Radio/>} label="En"/>*/}
-        {/*</RadioGroup>*/}
+        <RadioSet className={css.langs} value={lang} onChange={e => changeLang(e)}>
+          <RadioSetItem value="fr">Fr</RadioSetItem>
+          <RadioSetItem value="en">En</RadioSetItem>
+        </RadioSet>
         <Tooltip title="Sources code">
           <Button variant="outlined" color="primary">
             <Icon className="fab fa-github"/>
@@ -102,13 +86,6 @@ const App = () => {
             <Icon>cloud_download</Icon>
           </Button>
         </Tooltip>
-
-        {/*<Fab size="medium" color="primary" aria-label="Sources">*/}
-        {/*<Icon className="fab fa-github"/>*/}
-        {/*</Fab>*/}
-        {/*<Fab size="medium" onClick={() => window.print()} color="primary" aria-label="Download as PDF / Print">*/}
-        {/*<Icon>cloud_download</Icon>*/}
-        {/*</Fab>*/}
       </div>
 
       <div className={css.body}>
