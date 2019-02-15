c74bb5f2dd38e4ff70df50eedd9de20a1965c78b
Alexandre <alexandre.annic1@gmail.com>
2019-02-15 22:40:25 +0100

    Translate french

--- src/i18n/cv.fr.js
+++ src/i18n/cv.fr.js
@@ -1,154 +1,153 @@
 export const cvFr = {
-  education: [
-    {
-      title: 'Master STL (Science et Technologie du Logiciel)',
-      honor: 'With high honours',
-      period: '2016',
-      location: 'UPMC (University Pierre et Marie Curie) - Paris',
-      logo: 'upmc.png',
-      content: `
-        Algorithmic (compression, hash methods, 2D geometry) -
-        Multithreaded and distributed programming
+  job: 'Ingénieur Full-stack',
+  location: 'Paris, France',
+  education: {
+    label: 'Éducation',
+    articles: [
+      {
+        title: 'Master STL (Science et Technologie du Logiciel)',
+        honor: 'Mention Bien',
+        period: '2016',
+        location: 'UPMC (Université Pierre et Marie Curie) - Paris',
+        logo: 'upmc.png',
+        content: `
+        Algorithmie (compression, hachage, géometrie 2D) -
+        Programmation concurrente et distribuée
         (<code>Scala</code>, <code>OCaml</code>, <code>Java</code>, <code>C</code>) -
-        Web app (security, tracking)
-    `
-    },
-    {
+        Web (securité, traquage)
+      `
+      },
+      {
 
-      title: 'Bachelor in computer science',
-      honor: 'With high honours',
-      period: '2014',
-      location: 'UPMC (University Pierre et Marie Curie) - Paris',
-      content: `
-        Algorithmic (complexity, data structure, recursion) -
+        title: 'Licence Informatique',
+        honor: 'Mention Bien',
+        period: '2014',
+        location: 'UPMC (Université Pierre et Marie Curie) - Paris',
+        content: `
+        Algorithmie (complexité, structure de données, recursion) -
         OOP - Designs patterns -
-        Databases (transactions, <code>SQL3</code>, <code>PL/SQL</code>, <code>NoSQL</code>, <code>XPath</code>)`,
-    },
-    {
-      title: 'DUT of computer science', //  (two-year university degree in technology
-      period: '2013',
-      location: 'IUT d\'Orsay',
-      logo: 'logo.jpg',
-      content: `
-        Unix architecture, bash, script -
+        Base de données (transactions, <code>SQL3</code>, <code>PL/SQL</code>, <code>NoSQL</code>, <code>XPath</code>)`,
+      },
+      {
+        title: 'DUT Informatique',
+        period: '2013',
+        location: 'IUT d\'Orsay',
+        logo: 'logo.jpg',
+        content: `
+        Architecture Unix, bash, script -
         POO <code>Java</code>, <code>C++</code> -
-        Database -
-        System programming in C (pipe, signal, fork, thread) -
-        Networking (DHCP, DNS, Firewall) -
-        Computer architecture
+        <code>SQL</code> -
+        Programmation système en <code>C</code> (pipe, signal, fork, thread) -
+        Réseau (DHCP, DNS, Firewall) -
+        Logique
       `,
-    },
-  ],
-  work: [
-
-    {
-      logo: 'particeep.jpg',
-      title: 'Full-stack engineer',
-      location: 'Particeep - Paris',
-      period: '2016 - 2019"',
-      content: `
-            Led front team and created several applications from scratch
-            in <code>React</code>/<code>Angular</code>, <code>Scala</code> and with continuous integration
-            with <code>Jenkins</code> as <b>Medef accélerateur</b>.<br/>
-            Contributing to evolution of a REST and a <code>GraphQL</code> API wrote in <code>Scala</code>.<br/>
-            Developed SDK for <code>JavaScript</code>, <code>PHP</code> and <code>Scala</code>.<br/>
-            Organized workshops for my team on topics like <code>JavaScript</code>-<code>CSS</code> good practices
-            and performances, <code>Angular2</code> or again <code>flexbox</code>.
-          `
-    },
-    {
-      logo: 'particeep.jpg',
-      title: 'Full-stack developer internship',
-      location: 'Particeep - Paris',
-      period: '2016',
-      content: `
-        Contributing to evolution of a large SaaS application wrote in <code>Play framework
-        1</code> and <code>AngularJS</code>. I refactored a large part to improve
-        performances, code readability and
-        reusability. Developing features as theme personalization, variabilized mails edition and form creation.
+      },
+    ]
+  },
+  work: {
+    label: 'Expériences',
+    articles: [
+      {
+        logo: 'particeep.jpg',
+        title: 'Ingénieur logiciel',
+        location: 'Particeep - Paris',
+        period: '2016 - 2019',
+        content: `
+        Direction de l'équipe front et création d'applications en <code>React</code>/<code>Angular</code>.<br/>
+        Contribution à l'évolution d'APIs REST et <code>GraphQL</code> codées en <code>Scala</code>.<br/>
+        Dévelopement de SDK <code>JavaScript</code>, <code>PHP</code> et <code>Scala</code>.<br/>
+        Organisation de workshops concernant le développement front.
       `
-    },
-    {
-      logo: 'activa.png',
-      title: 'Full-stack developer',
-      location: 'Activa Informatique - Paris',
-      period: 'Summers 2013, 2014 and 2015',
-      content: `
-        Developping features on a SaaS application like appointments calendar, real-time chat, statistics and
-        refactoring front side with the following stack:&nbsp;
-        <code>Play framework 1</code>, <code>JPA</code>, <code>JQuery</code>
-        <br/>
-        Developing couple websites in <code>PHP</code> for association or hotel.
+      },
+      {
+        logo: 'particeep.jpg',
+        title: 'Stage développeur logiciel',
+        location: 'Particeep - Paris',
+        period: '2016',
+        content: `
+        Contribution au développement d'une application SaaS codée en <code>Java</code> et <code>AngularJS</code>.<br/>
+        Réecriture de l'ensemble de l'application pour améliorer les performances, la lisibilité et la réutilisabilité du code.<br/>
+        Développement de fonctionnalités telles que la personnalisation de l'apparence et l'édition de mails.
       `
-    },
-    {
-      logo: 'upmc.png',
-      title: 'Semester student project',
-      location: 'UPMC (University Pierre et Marie Curie) - Paris',
-      period: '6 months - 2015',
-      content: `
-        Based on an application of distributed components, my goal was to implement a distributed registry inspired
-        by DHT allowing to "colonize" the machines of a network to avoid requests saturation.<br/>
-        <code>Java</code> (Future, Socket, multithreading, RMI) and scripts <code>Perl</code>.
+      },
+      {
+        logo: 'upmc.png',
+        title: 'Ingénieur logiciel',
+        location: 'Event Catalyst',
+        period: '6 mois - 2016',
+        content: `
+          Développement d'un système d'organisation de conférences, comprenant une API <code>ASP.NET</code>,
+          une application <code>ASP.NET</code> et Android.<br/>
+          Fonctionnalités: check-in QR-code, formulaire éditable, gestion des visiteurs...
+       `
+      },
+      {
+        logo: 'activa.png',
+        title: 'Développeur logiciel',
+        location: 'Activa Informatique - Paris',
+        period: 'Étés 2013, 2014 and 2015',
+        content: `
+        Développement de fonctionnalités sur une application SaaS codée en <code>Java</code>, <code>JPA</code> et <code>JQuery</code> 
+        telles que: calendrier de rendez-vous, discussion instantanée, statistiques.<br/>
+        Création de sites web en <code>PHP</code> pour des PME et des associations. 
       `
-    },
-    {
-      title: 'Internship in computer research',
-      location: 'LRI (Computer Research Laboratory) - Orsay',
-      period: '3 months - 2013',
-      content: `
-        I worked for the Grid Observatory project whose purpose is to contribute to an experimental theory of large
-        grid
-        systems by integrating the collection of data on the behaviour of the flagship European Grid Infrastructure.
-        My mission was to reconstruct the grid usage traces to make them accessible to researchers.<br/>
-        <code>Perl</code> <code>SQL</code> <code>Script shell</code>
+      },
+      {
+        logo: 'upmc.png',
+        title: 'Projet de Master 1',
+        location: 'UPMC (Université Pierre et Marie Curie) - Paris',
+        period: '6 mois - 2015',
+        content: `
+          Développement d'un registre auto-adaptatif inspiré du principe des DHT pour une application distribuée.
+          Technologies: Future, Socket, Thread et RMI. 
+        `
+      },
+      {
+        title: 'Stage de recherche informatique',
+        location: 'LRI (au Laboratoire de Recherche Informatique) - Orsay',
+        period: '3 mois - 2013',
+        content: `
+        Conception d’un système de reconstruction de données provenant de plus de
+        250 machines associées à la grille EGI (traitement de +200Go de données par mois).
       `
-    },
-    {
-      logo: 'upmc.png',
-      title: 'Création d\'une application concurrente et répartie',
-      location: 'Projet universitaire de Master 1',
-      period: '2 mois - 2015',
-      content: `
-          Permet à des musiciens d'effectuer des jams sessions en temps réel.
-          Développement du serveur <code>TCP/IP</code> en <code>C</code> redistribuant les sons
-          mixés selon les musiciens et anticipant le délai et les instabilités du réseau.
-          Développement du client en <code>Java</code> avec interface Swing (inscription, gestion des
-          salons, jouer).
-         `
-    },
-
-  ],
+      },
+    ]
+  },
   skills: [
     {
       title: 'Framework',
       content: [
         {
-          logo: "dev/angular.png",
-          title: "Angular (2+)",
-          rate: 5,
-          content: `
-              Used in larges applications (~200 components) sometimes coupled <code>ngrx</code>.
-            `,
-        },
-        {
           logo: "dev/react.png",
           title: "React",
           rate: 5,
           content: `
-              Used in large application (+100 components) sometimes coupled
-              with <code>Redux</code> and <code>TypeScript</code>.
-            `
+            Utilisé dans de grandes applications avec <code>Redux</code> et <code>TypeScript</code>
+          `
+        },
+        {
+          logo: "dev/angular.png",
+          title: "Angular (2+)",
+          rate: 5,
+          content: `
+            Utilisé dans de grande applications parfois avec <code>ngrx</code>.
+          `,
         },
         {
           logo: "dev/angularjs.png",
           title: "AngularJS",
-          rate: 4
+          rate: 4,
+          content: `
+            
+          `
         },
         {
           logo: "dev/play.png",
           title: "Play framework",
-          rate: 4
+          rate: 4,
+          content: `
+            Utilisation des 2 versions pour des applications SaSS et des API REST et GraphQL
+          `
         }
       ]
     },
@@ -181,7 +180,7 @@ export const cvFr = {
           rate: 2.5,
         },
         {
-          logo: "",
+          logo: "dev/php.png",
           title: "PHP",
           rate: 4,
         },
@@ -200,14 +199,26 @@ export const cvFr = {
     {
       title: 'Persistence',
       content: [
-        {title: 'SQL', rate: 4.5},
-        {title: 'NoSQL', rate: 3},
+        {
+          logo: 'dev/sql.png',
+          title: 'SQL',
+          rate: 4.5,
+          // content: `
+          //   Mainly <code>PostGres</code> anad <code>MySQL</code>. Script <code>PL/SQL</code>
+          // `
+        },
+        {
+          logo: 'dev/nosql.png',
+          title: 'NoSQL',
+          rate: 3,
+          // content: `Used document-oriented databases mainly during studies projects`
+        },
       ]
     }
   ],
   various: [
-    'Diploma for youth leaders and workers (BAFA)',
-    'Sport: Crossfit, climbing, running',
-    'Driving licence',
+    'BAFA',
+    `<div style="white-space: nowrap">Sport quotidien: crossfit, escalade</div>`,
+    'Permis B',
   ]
 }
